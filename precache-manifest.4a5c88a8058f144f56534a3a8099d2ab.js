6a42d4f5ba5a296d282e47cc965e39c5b1dba7ef
Vadim Demidov <vadim7552@yandex.ru>
2018-11-09 14:19:28 +0300

    Updates

---
+++ precache-manifest.4a5c88a8058f144f56534a3a8099d2ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3bf3d519fe373d61948",
+    "url": "/book-store/static/js/runtime~main.b3bf3d51.js"
+  },
+  {
+    "revision": "e186ce207fb466cc958e",
+    "url": "/book-store/static/js/main.e186ce20.chunk.js"
+  },
+  {
+    "revision": "8347dbbf8296b6e021be",
+    "url": "/book-store/static/js/1.8347dbbf.chunk.js"
+  },
+  {
+    "revision": "e186ce207fb466cc958e",
+    "url": "/book-store/static/css/main.6e550b91.chunk.css"
+  },
+  {
+    "revision": "a472ca8ce242b64880425703f4ee179f",
+    "url": "/book-store/index.html"
+  }
+];
